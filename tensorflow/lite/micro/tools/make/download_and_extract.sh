5bd4da816c82f81a04811e06c76dd6c3b78760b0
Kamil Rakoczy <krakoczy@antmicro.com>
2020-03-20 12:44:59 +0100

    Add zephyr setup

--- tensorflow/lite/micro/tools/make/download_and_extract.sh
+++ tensorflow/lite/micro/tools/make/download_and_extract.sh
@@ -86,6 +86,16 @@ build_embarc_mli() {
   gmake -j 4 -C ${1}/lib/make TCF_FILE=${2}
 }
 
+setup_zephyr() {
+  command -v virtualenv >/dev/null 2>&1 || {
+    echo >&2 "The required 'virtualenv' tool isn't installed. Try 'pip install virtualenv'."; exit 1;
+  }
+  cd ${1}
+  pip install -r scripts/requirements.txt
+  west init -m https://github.com/zephyrproject-rtos/zephyr.git
+  cd -
+}
+
 # Main function handling the download, verify, extract, and patch process.
 download_and_extract() {
   local usage="Usage: download_and_extract URL MD5 DIR [ACTION] [ACTION_PARAM]"
@@ -161,6 +171,8 @@ download_and_extract() {
     patch_kissfft ${dir}
   elif [[ ${action} == "build_embarc_mli" ]]; then
     build_embarc_mli ${dir} ${action_param1}
+  elif [[ ${action} == "setup_zephyr" ]]; then
+    setup_zephyr ${dir}
   elif [[ ${action} ]]; then
     echo "Unknown action '${action}'"
     exit 1
